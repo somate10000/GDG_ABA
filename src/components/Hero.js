c7528c85036994a98ab93a04a990381d8fc8c871
somate10000 <98242447+somate10000@users.noreply.github.com>
2022-07-06 05:28:22 -0700

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,10 +6,16 @@ const Hero = () => {
   return (
     <div className="flex bg-gray-50 h-[100%] items-center justify-center pb-32">
       <div className="items-center justify-center w-full mt-52">
-        <div className="flex justify-center text-7xl">
-          <h1 className="text-blue-500">Connect.</h1>
-          <h1 className="text-red-500">Learn.</h1>
-          <h1 className="text-green-600">Grow.</h1>
+        <div className="relative flex justify-center text-7xl top-44">
+          <h1 id="pop-up" className="text-blue-500">
+            Connect.
+          </h1>
+          <h1 id="pop-up" className="text-red-500">
+            Learn.
+          </h1>
+          <h1 id="pop-up" className="text-green-600">
+            Grow.
+          </h1>
         </div>
         <div className="flex items-center justify-center   h-[550px] ">
           <img src={blueCircle} className="relative top-8" alt="" />
